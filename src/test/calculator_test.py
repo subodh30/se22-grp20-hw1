8b0f31d5bb50476363df27d8d2a0e83bced03393
subodhgujar <subodhsgujar@gmail.com>
2022-08-28 20:35:43 -0400

    Added functions and tests.

---
+++ src/test/calculator_test.py
@@ -0,0 +1,25 @@
+from src.code.calculator import addition, subtraction, division, multiplication, mod, power
+
+
+def test_addition():
+    assert addition(9, 9) == 18
+
+
+def test_subtraction():
+    assert subtraction(9, 9) == 0
+
+
+def test_multiplication():
+    assert multiplication(9, 9) == 81
+
+
+def test_division():
+    assert division(9, 9) == 1
+
+
+def test_modulus():
+    assert mod(9, 9) == 0
+
+
+def test_power():
+    assert power(9, 2) == 81
